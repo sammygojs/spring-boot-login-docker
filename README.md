bb60d6447e8380fa914f7877238432b38dad868f
Sumit <akoliyasumit@hotmail.com>
2024-05-20 17:03:26 +0100

    Adding AuthController

--- README.md
+++ README.md
@@ -79,3 +79,27 @@ return new BCryptPasswordEncoder();
 
 Solved it.
 
+6. Adding AuthController in Controller pkg
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestParam String email, @RequestParam String password) {
+        return userService.loginUser(email, password);
+    }
+}
+
+
---
+++ src/main/java/com/sammygojs/springbootlogindocker/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.sammygojs.springbootlogindocker.controller;
+
+import com.sammygojs.springbootlogindocker.model.User;
+import com.sammygojs.springbootlogindocker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestParam String email, @RequestParam String password) {
+        return userService.loginUser(email, password);
+    }
+}
