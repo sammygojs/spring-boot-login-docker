fcce67cab9986f66e91cc93a7a225e173199915d
Sumit <akoliyasumit@hotmail.com>
2024-05-20 16:46:54 +0100

    Project Init

---
+++ src/main/java/com/sammygojs/springbootlogindocker/SpringBootLoginDockerApplication.java
@@ -0,0 +1,13 @@
+package com.sammygojs.springbootlogindocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoginDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoginDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sammygojs/springbootlogindocker/SpringBootLoginDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sammygojs.springbootlogindocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoginDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
